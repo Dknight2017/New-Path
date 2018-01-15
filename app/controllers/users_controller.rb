a9205537d7447502ed6b6337179b9c82e019cfa5
Dylan Knight <samuraiknight713@gmail.com>
2018-01-15 15:57:25 -0800

    third-session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,12 @@ class UsersController < ApplicationController
   end
 
   def new
-    @users = User.new
+    @user = User.new
   end
 
   def create
-    @users = User.new(user_params)
-    if @users.save
+    @user = User.new(user_params)
+    if @user.save
       redirect_to "/users"
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get '/login' => 'sessions#new', as: :login
-  post '/login' => 'sessions#create'
+
   delete '/logout' => 'sessions#destroy', as: :logout
+  resources :sessions, only: [:new, :create]
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
